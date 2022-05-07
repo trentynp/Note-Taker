16edbb4c48231a29e8b2c0efdc7bfb2568185412
Trentyn <trentynpowell@gmail.com>
2022-05-07 11:39:05 -0700

    server apps

--- server.js
+++ server.js
@@ -5,3 +5,13 @@ const htmlRoutes = require('./routes/htmlRoutes');
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Now listening on PORT: ${PORT}`);
+});

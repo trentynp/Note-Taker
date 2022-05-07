25eb18346b329dedf4dfb93e823347a49dae340e
Trentyn <trentynpowell@gmail.com>
2022-05-07 11:58:25 -0700

    lets

---
+++ stuff/assets/js/index.js
@@ -0,0 +1,13 @@
+let noteTitle;
+let noteText;
+let saveNoteBtn;
+let newNoteBtn;
+let noteList;
+
+if (window.location.pathname === '/notes') {
+  noteTitle = document.querySelector('.note-title');
+  noteText = document.querySelector('.note-textarea');
+  saveNoteBtn = document.querySelector('.save-note');
+  newNoteBtn = document.querySelector('.new-note');
+  noteList = document.querySelectorAll('.list-container .list-group');
+}

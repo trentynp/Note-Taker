c25573298c8f87ab71cfc80230dea98918edad5c
Trentyn <trentynpowell@gmail.com>
2022-05-07 12:33:35 -0700

    heroku

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,11 +3,11 @@ const path = require('path');
 const router = require('express').Router();
 
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'))
+    res.sendFile(path.join(__dirname, '../stuff/notes.html'))
 })
 
 router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'))
+    res.sendFile(path.join(__dirname, '../stuff/index.html'))
 })
 
 module.exports = router;

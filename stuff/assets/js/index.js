a812f3565159f19ad7d07b8ac6bf67719b2e4820
Trentyn <trentynpowell@gmail.com>
2022-05-07 12:02:24 -0700

    hnadle note

--- stuff/assets/js/index.js
+++ stuff/assets/js/index.js
@@ -62,3 +62,15 @@ const renderActiveNote = () => {
     noteText.value = '';
   }
 };
+
+const handleNoteSave = () => {
+    const newNote = {
+      title: noteTitle.value,
+      text: noteText.value,
+    };
+    saveNote(newNote).then(() => {
+      getAndRenderNotes();
+      renderActiveNote();
+    });
+  };
+  

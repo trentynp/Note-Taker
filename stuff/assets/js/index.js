af8a03d970841e3a0a95d2b8a6efcb417d35acfa
Trentyn <trentynpowell@gmail.com>
2022-05-07 12:05:24 -0700

    return html element

--- stuff/assets/js/index.js
+++ stuff/assets/js/index.js
@@ -116,3 +116,47 @@ const handleNoteSave = () => {
     }
   
     let noteListItems = [];
+
+    const createLi = (text, delBtn = true) => {
+        const liEl = document.createElement('li');
+        liEl.classList.add('list-group-item');
+    
+        const spanEl = document.createElement('span');
+        spanEl.classList.add('list-item-title');
+        spanEl.innerText = text;
+        spanEl.addEventListener('click', handleNoteView);
+    
+        liEl.append(spanEl);
+    
+        if (delBtn) {
+          const delBtnEl = document.createElement('i');
+          delBtnEl.classList.add(
+            'fas',
+            'fa-trash-alt',
+            'float-right',
+            'text-danger',
+            'delete-note'
+          );
+          delBtnEl.addEventListener('click', handleNoteDelete);
+    
+          liEl.append(delBtnEl);
+        }
+    
+        return liEl;
+      };
+    
+      if (jsonNotes.length === 0) {
+        noteListItems.push(createLi('No saved Notes', false));
+      }
+    
+      jsonNotes.forEach((note) => {
+        const li = createLi(note.title);
+        li.dataset.note = JSON.stringify(note);
+    
+        noteListItems.push(li);
+      });
+    
+      if (window.location.pathname === '/notes') {
+        noteListItems.forEach((note) => noteList[0].append(note));
+      }
+    };

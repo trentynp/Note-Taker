0e3475d42b253aac447f86ef9ee67a85e9c956b1
Trentyn <trentynpowell@gmail.com>
2022-05-07 12:01:37 -0700

    save note

--- stuff/assets/js/index.js
+++ stuff/assets/js/index.js
@@ -29,3 +29,12 @@ const getNotes = () =>
       'Content-Type': 'application/json',
     },
   });
+
+  const saveNote = (note) =>
+  fetch('/api/notes', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(note),
+  });
